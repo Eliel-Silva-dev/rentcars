ff14f72fc4906b0764ce52851e6f48a268d48e2e
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-13 20:52:03 -0300

    feat: react rentcars environment

---
+++ src/shared/environment/index.ts
@@ -0,0 +1,10 @@
+export const Environment = {
+  /*
+    limite de linhas retornadas na requisição api
+  */
+  LIMITE_LINHAS_API: 10,
+  /*
+  url base para consultas api
+  */
+  URL_BASE: 'http://localhost:3333',
+};
