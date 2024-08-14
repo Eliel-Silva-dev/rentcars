ab99574243e13681b0db4becf399e9cbc44a2171
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-14 14:07:23 -0300

    feat: react rentcars carro services services getCarsById

--- src/shared/services/api/cars/CarsServices.ts
+++ src/shared/services/api/cars/CarsServices.ts
@@ -50,5 +50,22 @@ const getAllCars = async (
   }
 };
 
+const getCarsById = async (id: string): Promise<IDetalheCarros | Error> => {
+  try {
+    const { data } = await Api.get(`/cars?${id}`);
 
-export { getAllCars };
+    if (data) {
+      return data;
+    }
+
+    return new Error('Erro ao consultar o resgistro');
+  } catch (error) {
+    console.error(error);
+
+    return new Error(
+      (error as { message: string }).message || 'Erro ao consultar o registro',
+    );
+  }
+};
+
+export { getAllCars, getCarsById };
