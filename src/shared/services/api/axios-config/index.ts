879f2d576c6422b3cd63e82774e4ca685b8bf68e
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-14 13:42:56 -0300

    feat: react rentcars api axios instance

--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,3 +1,15 @@
-const Api = {};
+import axios from 'axios';
+
+import { errorInterceptor, respondeInterceptor } from './interceptors';
+import { Environment } from '@/shared/environment';
+
+const Api = axios.create({
+  baseURL: Environment.URL_BASE,
+});
+
+Api.interceptors.response.use(
+  (response) => respondeInterceptor(response),
+  (error) => errorInterceptor(error),
+);
 
 export { Api };
